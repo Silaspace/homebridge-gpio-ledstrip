d74dc252c01d3e3c865a9382951508743bf581f9
Silaspace <silasjharvey@gmail.com>
2021-01-03 15:59:08 +0000

    First new implementation test

--- src/accessory.ts
+++ src/accessory.ts
@@ -13,62 +13,58 @@ import {
 
 let hap: HAP;
 
-/*
- * Initializer function called when the plugin is loaded.
- */
 export = (api: API) => {
   hap = api.hap;
-  api.registerAccessory("ExampleSwitch", ExampleSwitch);
+  api.registerAccessory("Ledstrip", Ledstrip);
 };
 
-class ExampleSwitch implements AccessoryPlugin {
+
+
+class Ledstrip implements AccessoryPlugin {
 
   private readonly log: Logging;
   private readonly name: string;
-  private switchOn = false;
-
-  private readonly switchService: Service;
+  private readonly service: Service;
   private readonly informationService: Service;
 
+  private state = false;
+
+
+
   constructor(log: Logging, config: AccessoryConfig, api: API) {
+    
     this.log = log;
     this.name = config.name;
+    this.service = new hap.Service.Lightbulb(this.name);
 
-    this.switchService = new hap.Service.Switch(this.name);
-    this.switchService.getCharacteristic(hap.Characteristic.On)
+    this.service.getCharacteristic(hap.Characteristic.On)
       .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
-        callback(undefined, this.switchOn);
+        log.info("Current state of the switch was returned: " + (this.state? "ON": "OFF"));
+        callback(undefined, this.state);
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.switchOn = value as boolean;
-        log.info("Switch state was set to: " + (this.switchOn? "ON": "OFF"));
+        this.state = value as boolean;
+        log.info("Switch state was set to: " + (this.state? "ON": "OFF"));
         callback();
       });
 
     this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
-      .setCharacteristic(hap.Characteristic.Model, "Custom Model");
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Generic LED")
+      .setCharacteristic(hap.Characteristic.Model, "Generic LED");
 
-    log.info("Switch finished initializing!");
+    log.info("Initialisation complete");
   }
 
-  /*
-   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-   * Typical this only ever happens at the pairing process.
-   */
+
+
   identify(): void {
-    this.log("Identify!");
+    this.log("Identify");
   }
 
-  /*
-   * This method is called directly after creation of this instance.
-   * It should return all services which should be added to the accessory.
-   */
   getServices(): Service[] {
     return [
       this.informationService,
-      this.switchService,
+      this.service,
     ];
   }
 
